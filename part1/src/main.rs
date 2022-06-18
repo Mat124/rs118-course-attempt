392ed37221cbc5572764a514cea8e933e957a896
Matthew Knight <68224419+Mat124@users.noreply.github.com>
2022-06-18 20:56:03 +0100

    started first lab

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -1,3 +1,49 @@
+#![allow(unused)]
+
+struct Professor {
+    name: String,
+    id: String,
+    subject: String,
+    parking_space: bool,
+    age: u32,
+    bald: bool,
+    hair: HairColour
+}
+
+enum HairColour {
+    Red,
+    Blonde,
+    Brown,
+    Black
+}
+
+enum Option<T> {
+    None,
+    Some(T)
+}
+
+fn getHair(x: Professor) -> Option<String> {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = '7';
+    println!("Hello, world! The value of x is {}", x);
+    x = 'd';
+    println!("The value of x is now {}", x);
+
+    let tup_one = (1, 2);
+    println!("The first value in tup_one is: {}\nthe second value is: {}", tup_one.0, tup_one.1);
+
+    let matt = Professor {
+        name: String::from("Matt"),
+        id: String::from("1234567"),
+        subject: String::from("Computer Science"),
+        parking_space: true,
+        age: 30,
+        bald: true,
+        hair: None
+    };
+
+    println!("The new professor is called {} and they are {} years old!", matt.name, matt.age);
 }
---
+++ part1/target/debug/deps/part1-e637fe597a0114ca.d
@@ -0,0 +1,5 @@
+/home/matthewknight/Documents/GitHub/rs118-course/part1/target/debug/deps/part1-e637fe597a0114ca.rmeta: src/main.rs
+
+/home/matthewknight/Documents/GitHub/rs118-course/part1/target/debug/deps/part1-e637fe597a0114ca.d: src/main.rs
+
+src/main.rs:
