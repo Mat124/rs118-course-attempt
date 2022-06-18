4e19efc9cc0bddc7388daf730458b962ec0203d5
Matthew Knight <68224419+Mat124@users.noreply.github.com>
2022-06-18 09:20:34 +0100

    we started, and i might have committed the environment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rs118-course
- my attempts at Joeyh021's Rust course
+
+my attempts at [Joeyh021's Rust course](https://joeyh.dev/rs118/intro.html) for UWCS
---
+++ part1/target/debug/part1.d
@@ -0,0 +1,1 @@
+/home/matthewknight/Documents/GitHub/rs118-course/part1/target/debug/part1: /home/matthewknight/Documents/GitHub/rs118-course/part1/src/main.rs
