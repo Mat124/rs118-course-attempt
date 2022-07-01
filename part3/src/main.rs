ccec14841e7022de11719431830c7e8a28be3ca7
Matthew Knight <68224419+Mat124@users.noreply.github.com>
2022-07-01 11:48:50 +0100

    start part3

--- part3/src/main.rs
+++ part3/src/main.rs
@@ -4,5 +4,14 @@ fn main() {
     //let i = v1[2]; //also creates a panic (error)
     
     // Results
-    
+    // Used to handle errors at runtime, either Ok or Err - used with match
+    use std::{fs, io};
+    let f1: Result<fs::File, io::Error> = fs::File::open("Cargo.toml"); //existant file
+    println!("{f1:?}");
+    let f2: Result<fs::File, io::Error> = fs::File::open("foo.bar"); //nonexistant file
+    println!("{f2:?}");
+
+    // Generics
+    // Exist similar to java's generics, very easy to use. Put in <> when initialising the variable
+
 }
---
+++ part3/target/debug/deps/part3-761339842a26210b.d
@@ -0,0 +1,5 @@
+/home/matthewknight/Documents/GitHub/rs118-course/part3/target/debug/deps/part3-761339842a26210b.rmeta: src/main.rs
+
+/home/matthewknight/Documents/GitHub/rs118-course/part3/target/debug/deps/part3-761339842a26210b.d: src/main.rs
+
+src/main.rs:
