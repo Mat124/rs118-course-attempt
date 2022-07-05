457773f955e03ab7acb9ea6b0959352f54682190
Matthew Knight <68224419+Mat124@users.noreply.github.com>
2022-07-05 20:39:15 +0100

    v minor changes

--- part3/src/main.rs
+++ part3/src/main.rs
@@ -10,6 +10,17 @@ fn main() {
     println!("{f1:?}");
     let f2: Result<fs::File, io::Error> = fs::File::open("foo.bar"); //nonexistant file
     println!("{f2:?}");
+    //can also use match
+    println!("Using match:");
+    match f1 {
+        Ok(file) => println!("File 1 opened successfully"),
+        Err(_) => println!("Could not open file 1")
+    }
+    match f2 {
+        Ok(file) => println!("File 2 opened successfully"),
+        Err(_) => println!("Could not open file 2")
+    }
+    //unwrap/expect would also work, but panics if Result is Err. Useful for prototyping
 
     // Generics
     // Exist similar to java's generics, very easy to use. Put in <> when initialising the variable
