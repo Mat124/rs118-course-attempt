a1e722d7343a70109f525ab8ca63ee261ccf347a
Matthew Knight <68224419+Mat124@users.noreply.github.com>
2022-08-04 13:27:11 +0100

    start chip8 impl

--- chip8/src/interpreter/mod.rs
+++ chip8/src/interpreter/mod.rs
@@ -1,4 +1,7 @@
-struct VM {
+use chip8_base::Interpreter;
+use std::time::Duration;
+
+pub struct VM {
     ram: [u8; 4096],
     reg: [u8; 16],
     stack: [u16; 16],
@@ -7,10 +10,27 @@ struct VM {
     delay: u8,
     pc: u16,
     sp: u8,
+    speed: std::time::Duration,
 }
 
 impl VM {
-    fn new() {
-        
+    pub fn new(hz: u32) -> VM {
+        let nanos = (1/hz) * 10 ^ 9;
+        VM { ram: ([0; 4096]), reg: ([0; 16]), stack: ([0; 16]), i: (0), sound: (0), delay: (0), pc: (0), sp: (0), speed: (Duration::new(0, nanos)) }
+    }
+}
+
+impl Interpreter for VM {
+    fn step(&mut self, keys: &chip8_base::Keys) -> Option<chip8_base::Display> {
+        //essentially fetch-decode-execute cycle
+        None
+    }
+
+    fn speed(&self) -> std::time::Duration {
+        self.speed
+    }
+
+    fn buzzer_active(&self) -> bool {
+        false
     }
 }
--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,5 +1,9 @@
 mod interpreter;
+use crate::interpreter::VM;
 
 fn main() {
-    println!("Hello, world!");
+    let VM = VM::new(700);
+
+    chip8_base::run(VM);
+
 }
