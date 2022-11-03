791e7fa66e911fe7c1f72aeb718f35150389b183
Matthew Knight <mrg.knight1@gmail.com>
2022-11-03 21:12:37 +0000

    start fetch/execute
    added logging and started writing decode/fetch match

--- chip8/src/interpreter/mod.rs
+++ chip8/src/interpreter/mod.rs
@@ -1,5 +1,6 @@
 use chip8_base::Interpreter;
 use std::time::Duration;
+use hex;
 
 pub struct VM {
     ram: [u8; 4096],
@@ -23,6 +24,8 @@ impl VM {
 impl Interpreter for VM {
     fn step(&mut self, keys: &chip8_base::Keys) -> Option<chip8_base::Display> {
         //essentially fetch-decode-execute cycle
+        let instruction = fetch(self);
+
         None
     }
 
@@ -32,5 +35,53 @@ impl Interpreter for VM {
 
     fn buzzer_active(&self) -> bool {
         false
+    }
+}
+
+fn fetch(interpreter: &mut VM) -> u16 {
+    let current = interpreter.pc;
+    interpreter.pc = (interpreter.pc + 2) % 4096; //maybe increment by 2 as each opcode is 2 bytes?
+    //need to combine 2 entries in ram into one
+    ((interpreter.ram[current as usize] as u16) << 8) | interpreter.ram[(current + 1) as usize] as u16
+}
+
+fn execute(interpreter: &mut VM, instruction: u16) {
+    let nibbles = ((instruction >> 12) as u8 ,(instruction >> 8) as u8 & 0x0F, (instruction as u8) >> 4, instruction as u8 & 0x0F);
+    match nibbles {
+        (0,0,0xE,0) => {},
+        (0,0,0xE,0xE) => {},
+        (1,_,_,_) => {},
+        (2,_,_,_) => {},
+        (3,_,_,_) => {},
+        (4,_,_,_) => {},
+        (5,_,_,0) => {},
+        (6,_,_,_) => {},
+        (7,_,_,_) => {},
+        (8,_,_,0) => {},
+        (8,_,_,1) => {},
+        (8,_,_,2) => {},
+        (8,_,_,3) => {},
+        (8,_,_,4) => {},
+        (8,_,_,5) => {},
+        (8,_,_,6) => {},
+        (8,_,_,7) => {},
+        (8,_,_,0xE) => {},
+        (9,_,_,0) => {},
+        (0xA,_,_,_) => {},
+        (0xB,_,_,_) => {},
+        (0xC,_,_,_) => {},
+        (0xD,_,_,_) => {},
+        (0xE,_,9,0xE) => {},
+        (0xE,_,0xA,1) => {},
+        (0xF,_,0,7) => {},
+        (0xF,_,0,0xA) => {},
+        (0xF,_,1,5) => {},
+        (0xF,_,1,8) => {},
+        (0xF,_,1,0xE) => {},
+        (0xF,_,2,9) => {},
+        (0xF,_,3,3) => {},
+        (0xF,_,5,5) => {},
+        (0xF,_,6,5) => {},
+        _ => {}
     }
 }
--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,9 +1,13 @@
 mod interpreter;
 use crate::interpreter::VM;
+use log::{debug, error, log_enabled, info, Level};
 
 fn main() {
-    let VM = VM::new(700);
+    env_logger::init();
 
-    chip8_base::run(VM);
+    let vm = VM::new(700);
 
+    chip8_base::run(vm);
+
+    
 }
